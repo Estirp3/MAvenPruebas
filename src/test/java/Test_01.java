d089feab6f5ca5b72476c7764f065d2a0f8427db
Estirp3 <patricio.calderonig@gmail.com>
2020-03-30 12:54:56 -0300

    Se sube Cambios

---
+++ src/test/java/Test_01.java
@@ -0,0 +1,47 @@
+import PO.PaginaInicio;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.PropertyConfigurator;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static Test.testMethods.takeSnapShot;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class Test_01 {
+
+    String log4jConfPath = "./src/test/resources/log4j.properties";
+    WebDriver driver;
+    PaginaInicio pagini;
+
+    @BeforeAll
+    public void SetUp() {
+        PropertyConfigurator.configure(log4jConfPath);
+        System.setProperty("java.net.useSystemProxies", "true");
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        this.pagini = new PaginaInicio(driver);
+    }
+
+    @AfterAll
+    public void tear(){
+        driver.close();
+    }
+
+    @AfterEach
+    public void afterEach(TestInfo testinfo) throws Exception {
+        takeSnapShot(this.driver, this.getClass().getName()+"-"+testinfo.getDisplayName());
+    }
+
+    @Test
+    @Order(1)
+    public void Paso1() {
+        pagini.IngresoPagina();
+    }
+
+
+}
